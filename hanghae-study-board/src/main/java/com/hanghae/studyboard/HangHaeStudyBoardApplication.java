8810b9a08cb8a0fe58eb6a17f4e842dccc25c5f5
waves0217 <waves0217@gmail.com>
2024-11-15 00:02:18 +0900

    프로젝트 초기 세팅

---
+++ hanghae-study-board/src/main/java/com/hanghae/studyboard/HangHaeStudyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.studyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangHaeStudyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangHaeStudyBoardApplication.class, args);
+	}
+
+}
---
+++ hanghae-study-board/src/test/java/com/hanghae/studyboard/HangHaeStudyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.studyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangHaeStudyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
